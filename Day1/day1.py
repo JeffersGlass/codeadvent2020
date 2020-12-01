e067ae63d811aec7930aa1cb33ac64114bc01b1a
Jeff Glass <glass.jeffrey@gmail.com>
2020-12-01 09:52:39 -0600

    Refactor code into functions for ease of adding other methods
    Signed-off-by: Jeff Glass <glass.jeffrey@gmail.com>

--- Day1/day1.py
+++ Day1/day1.py
@@ -2,16 +2,24 @@ from sys import exit
 
 file = 'input.txt'
 
-data = list()
 
-with open(file, 'r') as infile:
-    for line in infile:
-        data.append(int(line.strip('\n').strip('\r')))
+def naiveFind():
+    data = list()
 
-for i in range(len(data)):
-    for j in range(i, len(data)):
-        if data[i] + data[j] == 2020:
-            print(f"Found that {data[i]} and {data[j]} sum to 2020; their product is {data[i]*data[j]}")
-            exit()
+    with open(file, 'r') as infile:
+        for line in infile:
+            data.append(int(line.strip('\n').strip('\r')))
 
-print ("Finished loop, no pairs found that sum to 2020")
+    for i in range(len(data)):
+        for j in range(i, len(data)):
+            if data[i] + data[j] == 2020:
+                success(data[i], data[j])
+                exit()
+
+    print ("Finished loop, no pairs found that sum to 2020")
+
+def success(first, second):
+    print(f"Found that {first} and {second} sum to 2020; their product is {first*second}")
+
+if __name__ == '__main__':
+    naiveFind()
