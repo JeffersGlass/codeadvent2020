3bcbc7805e51bc24ec8d5304442e2d36b32372fe
Jeff Glass <glass.jeffrey@gmail.com>
2020-12-01 09:50:02 -0600

    Basic implementation of solution looping over all entries like bubble sort
    Signed-off-by: Jeff Glass <glass.jeffrey@gmail.com>

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,9 +1,17 @@
+from sys import exit
+
 file = 'input.txt'
 
 data = list()
 
-with open('input.txt', 'r') as infile:
+with open(file, 'r') as infile:
     for line in infile:
-        data.append(line.strip('\n').strip('\r'))
+        data.append(int(line.strip('\n').strip('\r')))
 
-print(data)
+for i in range(len(data)):
+    for j in range(i, len(data)):
+        if data[i] + data[j] == 2020:
+            print(f"Found that {data[i]} and {data[j]} sum to 2020; their product is {data[i]*data[j]}")
+            exit()
+
+print ("Finished loop, no pairs found that sum to 2020")
