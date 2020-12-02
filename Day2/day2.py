964f7f4864805bbfceb4564b325e68bfc6f8378f
Jeff Glass <glass.jeffrey@gmail.com>
2020-12-02 10:07:28 -0600

    Add print statement for solution to part 2
    Signed-off-by: Jeff Glass <glass.jeffrey@gmail.com>

--- Day2/day2.py
+++ Day2/day2.py
@@ -1,6 +1,10 @@
 def isValid_Sled(low, high, letter, password):
     return low <= password.count(letter) <= high
 
+#The 'first', 'second' inputs are given as 1-indexed values per the problem statement
+def isValid_Toboggan(first, second, letter, password):
+    return (password[first-1] == letter) ^ (password[second-1] == letter)
+
 def isValidLine(line, func):
     contents = line.split(' ')
     lowbound, highbound = [int(x) for x in contents[0].split('-')]
@@ -8,12 +12,8 @@ def isValidLine(line, func):
     password = contents[2].strip("\n")
     return func(lowbound, highbound, letter, password)
 
-#While the problem statements/input give input with '1-indexed' values, this function takes '0-indexed' values
-def isValid_Toboggan(first, second, letter, password):
-    return (password[first] == letter) ^ (password[second] == letter)
-
-
 with open('input.txt', 'r') as infile:
     data = [line for line in infile]
 
 print ("The solution to part 1 is: " + str(len([line for line in data if isValidLine(line, isValid_Sled)])))
+print ("The solution to part 2 is: " + str(len([line for line in data if isValidLine(line, isValid_Toboggan)])))
