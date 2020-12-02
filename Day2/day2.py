f779c6c0e97074ea3fe506403c672af9d35b0051
Jeff Glass <glass.jeffrey@gmail.com>
2020-12-02 10:04:56 -0600

    Refactor isValidLine to accept a validation function
    Allows us to re-use line-splitting code
    
    Signed-off-by: Jeff Glass <glass.jeffrey@gmail.com>

--- Day2/day2.py
+++ Day2/day2.py
@@ -1,12 +1,12 @@
 def isValid_Sled(low, high, letter, password):
     return low <= password.count(letter) <= high
 
-def isValidLine_Sled(line):
+def isValidLine(line, func):
     contents = line.split(' ')
     lowbound, highbound = [int(x) for x in contents[0].split('-')]
     letter = contents[1].strip(":")
     password = contents[2].strip("\n")
-    return isValid_Sled(lowbound, highbound, letter, password)
+    return func(lowbound, highbound, letter, password)
 
 #While the problem statements/input give input with '1-indexed' values, this function takes '0-indexed' values
 def isValid_Toboggan(first, second, letter, password):
@@ -16,4 +16,4 @@ def isValid_Toboggan(first, second, letter, password):
 with open('input.txt', 'r') as infile:
     data = [line for line in infile]
 
-print ("The solution to part 1 is: " + str(len([line for line in data if isValidLine_Sled(line)])))
+print ("The solution to part 1 is: " + str(len([line for line in data if isValidLine(line, isValid_Sled)])))
