f80810186ee54d4d93f8656c8427a16740d0560c
Jeff Glass <glass.jeffrey@gmail.com>
2020-12-02 09:58:36 -0600

    Create solution to day one using pythonic count statement
    Signed-off-by: Jeff Glass <glass.jeffrey@gmail.com>

--- Day2/day2.py
+++ Day2/day2.py
@@ -1,16 +1,15 @@
 def isValid(low, high, letter, password):
     return low <= password.count(letter) <= high
 
+def isValidLine(line):
+    contents = line.split(' ')
+    lowbound, highbound = [int(x) for x in contents[0].split('-')]
+    letter = contents[1].strip(":")
+    password = contents[2].strip("\n")
+    return isValid(lowbound, highbound, letter, password)
+
+
 with open('input.txt', 'r') as infile:
     data = [line for line in infile]
 
-sum = 0
-contents = data[4].split(' ')
-print(contents)
-lowbound, highbound = [int(x) for x in contents[0].split('-')]
-print (f"{lowbound} {highbound}")
-letter = contents[1].strip(":")
-print(letter)
-password = contents[2].strip("\n")
-print(password)
-print (isValid(lowbound, highbound, letter, password))
+print (len([line for line in data if isValidLine(line)]))
