4b60a971e3c913fb728b983417c7adc7d74c1c60
Jeff Glass <glass.jeffrey@gmail.com>
2020-12-08 10:58:50 -0600

    Refactor part 1 solution as function

--- Day8/day8.py
+++ Day8/day8.py
@@ -1,26 +1,28 @@
-data = list()
+inputData = list()
 
 with open('input.txt', 'r') as infile:
     for line in infile:
         inst, val = line.split(' ')[0], int(line.split(' ')[1])
-        data.append((inst, val))
+        inputData.append((inst, val))
     
+def accumulatorValueBeforeLoop(data):
 
-accumulator = 0
-head = 0
-seenInstructions = set()
+    accumulator = 0
+    head = 0
+    seenInstructions = set()
 
-while head not in seenInstructions:
-    seenInstructions.add(head)
-    print(f"Processing instruction @ {head}: {data[head][0]},{data[head][1]}")
-    if data[head][0] == 'nop':
-        head += 1
-    elif data[head][0] == 'acc':
-        accumulator += data[head][1]
-        head += 1
-    elif data[head][0] == 'jmp':
-        head += data[head][1]
+    while head not in seenInstructions:
+        seenInstructions.add(head)
+        print(f"Processing instruction @ {head}: {data[head][0]},{data[head][1]}")
+        if data[head][0] == 'nop':
+            head += 1
+        elif data[head][0] == 'acc':
+            accumulator += data[head][1]
+            head += 1
+        elif data[head][0] == 'jmp':
+            head += data[head][1]
 
+    return accumulator
 
-print(accumulator)
+print(accumulatorValueBeforeLoop(inputData))
 
