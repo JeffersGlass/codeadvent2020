c51fad79b66cb793c6ff3cfcc04e528e25c263b6
Jeff Glass <glass.jeffrey@gmail.com>
2020-12-09 08:29:31 -0600

    Add solution to day 9 part 2

--- Day9/day9.py
+++ Day9/day9.py
@@ -6,9 +6,29 @@ def getFirstNonSum(lookback, data):
             for summand in data[i-lookback:i]] \
         ): return data[i]
 
+def findListSumTo(target, data):
+    startNum = 0
+    length = 1
+
+    while(True):
+        value = sum(data[startNum:startNum+length])
+        if value == target:
+            return data[startNum:startNum+length]
+        elif value > target:
+            startNum += 1
+            length = 1
+        else:
+            length += 1        
+
+
 if __name__ == '__main__':
 
     with open('input.txt', 'r') as infile:
         data = [int(line) for line in infile]
 
-    print(getFirstNonSum(25, data))
+    invalidNum = getFirstNonSum(25, data)
+    print(f"Solution to part 1 is: {invalidNum}")
+
+    sumList = findListSumTo(invalidNum, data)
+    print(f"Solution to part 2 is:  {max(sumList) + min(sumList)}")
+
