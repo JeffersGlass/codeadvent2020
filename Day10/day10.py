2f5257fbb2bf2181a1591e01365fa0bc78627188
Jeff Glass <glass.jeffrey@gmail.com>
2020-12-10 09:03:43 -0600

    Clean up solution to day 10 part 1

--- Day10/day10.py
+++ Day10/day10.py
@@ -2,7 +2,9 @@ with open('input.txt', 'r') as infile:
     data = [int(line) for line in infile]
     data += [0] + [max(data)+3]
 
-oneGaps = [d for d in data if d+1 in data]
-threeGaps = [d for d in data if (d+1 not in data and d+2 not in data and d+3 in data)]
-print(f"{len(oneGaps)},{len(threeGaps)}")
-print(f"Solution to part 1 is: {len(oneGaps) * len(threeGaps)}")
+if __name__ == '__main__':
+
+
+    oneGaps = [d for d in data if d+1 in data]
+    threeGaps = [d for d in data if (d+1 not in data and d+2 not in data and d+3 in data)]
+    print(f"Solution to part 1 is: {len(oneGaps) * len(threeGaps)}")
