b4beb353bf0e01315aaddbff405ca8908c0cd2e5
Jeff Glass <glass.jeffrey@gmail.com>
2020-12-10 09:01:33 -0600

    Add solution to day 10 part 1

---
+++ Day10/day10.py
@@ -0,0 +1,8 @@
+with open('input.txt', 'r') as infile:
+    data = [int(line) for line in infile]
+    data += [0] + [max(data)+3]
+
+oneGaps = [d for d in data if d+1 in data]
+threeGaps = [d for d in data if (d+1 not in data and d+2 not in data and d+3 in data)]
+print(f"{len(oneGaps)},{len(threeGaps)}")
+print(f"Solution to part 1 is: {len(oneGaps) * len(threeGaps)}")
