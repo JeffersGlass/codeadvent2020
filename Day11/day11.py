81b020f4be0e792eb9626c3ba71d781740f42a89
Jeff Glass <glass.jeffrey@gmail.com>
2020-12-11 10:43:55 -0600

    Move functional code into main method

--- Day11/day11.py
+++ Day11/day11.py
@@ -1,21 +1,5 @@
 from copy import deepcopy
 
-seats = dict()
-
-row = 0
-column = 0
-
-with open('input.txt', 'r') as infile:
-    for line in infile:
-        column = 0
-        for seat in line:
-            seats[(row, column)] = line[column]
-            column +=1
-        row += 1
-
-    seats['numRows'] = row
-    seats['numCols'] = column
-
 def printSeatingLayout(layout):
     for row in range(layout['numRows']):
         for column in range(layout['numCols']):
@@ -53,9 +37,25 @@ def seatsFilled(layout):
     return sum([(1 if layout[row, column] == '#' else 0) for row in range(layout['numRows']) for column in range(layout['numCols'])])
 
 if __name__ == '__main__':
-    oldLayout = deepcopy(seats)
+    seats = dict()
 
+    row = 0
+    column = 0
+
+    with open('input.txt', 'r') as infile:
+        for line in infile:
+            column = 0
+            for seat in line:
+                seats[(row, column)] = line[column]
+                column +=1
+            row += 1
+
+        seats['numRows'] = row
+        seats['numCols'] = column
+    
+    oldLayout = deepcopy(seats)
     seats = iterate(seats, getImmediateNeighbors)
+    
     while(oldLayout != seats):
         oldLayout = deepcopy(seats)
         seats = iterate(seats, getImmediateNeighbors)
