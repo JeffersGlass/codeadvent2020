4c029d4e516e45e26cfbcbdff13e2992639fad8a
Jeff Glass <glass.jeffrey@gmail.com>
2020-12-12 08:20:23 -0600

    Throw error if any turns are not 90-degree increments

--- Day12/day12.py
+++ Day12/day12.py
@@ -1,2 +1,5 @@
+#Check whether any turns in the input file are not 90-degree increments
 with open('input.txt', 'r') as infile:
-    print([line for line in infile if line[0] in ['L','R'] and line.strip()[1:] not in ['90','180','270']])
+    nonNinetyTurns = [line for line in infile if line[0] in ['L','R'] and line.strip()[1:] not in ['90','180','270']]
+    if len(nonNinetyTurns) > 0:
+        raise ValueError(f"One ore more turns is not an increment of 90 degrees; algorithm may give incorrect answers. {nonNinetyTurns}")
