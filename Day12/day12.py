ee27b01258c274f0e7c9d232e354cf57a31a5f47
Jeff Glass <glass.jeffrey@gmail.com>
2020-12-12 08:55:36 -0600

    Create functional solution to part 2

--- Day12/day12.py
+++ Day12/day12.py
@@ -8,6 +8,8 @@ with open(fileName, 'r') as infile:
 
 dirs = {'N':(0,-1), 'E':(1,0), 'W':(-1,0), 'S':(0,1)}
 
+#Part 1:
+
 location = [0,0]
 direction = list(dirs['E'])
 
@@ -27,5 +29,29 @@ with open(fileName, 'r') as infile:
             raise ValueError(f"Unknown command {line}")
         #input(f"Line {line.strip()} lead to location: {location}")
 
-print(location)
 print(f"Solution to part 1: {abs(location[0])+abs(location[1])}")
+
+#Part 2:
+
+location = [0,0]
+waypoint = [10,-1]
+direction = list(dirs['E'])
+
+with open(fileName, 'r') as infile:
+    for line in infile:
+        if line[0] in dirs:
+            waypoint = [a+b for a,b in zip(waypoint, [d*int(line[1:]) for d in dirs[line[0]]])]
+        elif line.strip() in ['R90', 'L270']:
+            waypoint = [-1*waypoint[1], waypoint[0]]
+        elif line.strip() in ['L90', 'R270']:
+            waypoint = [waypoint[1], -1*waypoint[0]]
+        elif line.strip() in ['R180', 'L180']:
+            waypoint = [-1*waypoint[0], -1*waypoint[1]]
+        elif line[0] == 'F':
+            location = [a+b for a,b in zip(location, [w*int(line[1:]) for w in waypoint])]
+        else:
+            raise ValueError(f"Unknown command {line}")
+        #input(f"Line {line.strip()} lead to location: {location}. Waypoint is {waypoint}.")
+
+print(location)
+print(f"Solution to part 2: {abs(location[0])+abs(location[1])}")
