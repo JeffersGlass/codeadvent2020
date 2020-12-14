e1917a452235aafe6b66ecc30f9afb058691860b
Jeff Glass <glass.jeffrey@gmail.com>
2020-12-14 08:46:03 -0600

    Create solution to part 1 of day 14

--- Day14.py/day14.py
+++ Day14.py/day14.py
@@ -1,5 +1,4 @@
-with open('inputtest.txt', 'r') as infile:
-    data = [line.strip() for line in infile]
+import re
 
 def applyMask(mask, num):
     onesMask =      int(''.join([('1' if m == '1' else '0')for m in mask ]), 2)
@@ -7,5 +6,26 @@ def applyMask(mask, num):
     num = num | onesMask
     num = num & zeroesMask
     return num
+
+if __name__ == '__main__':
+
+    with open('input.txt', 'r') as infile:
+        data = [line.strip() for line in infile]
+
+    #part1
+    mask = '0'
+    memory = dict()
+
+    for line in data:
+        if 'mask' in line:
+            mask = line.split(' = ')[1]
+        else:
+            pattern = r'\[([0-9]+)\] = ([0-9]+)'
+            m = re.search(pattern, line)
+            address = int(m.group(1))
+            value = int(m.group(2))
+            memory[address] = applyMask(mask, value)
+
+    print(f"Solution to part 1 is: {sum(memory.values())}")
 
     
