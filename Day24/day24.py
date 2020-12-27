ba10ca19801648081709edd44c8ce03fe76cb8cc
Jeff Glass <glass.jeffrey@gmail.com>
2020-12-26 23:12:25 -0600

    Create solution to Day24 part 1

---
+++ Day24/day24.py
@@ -0,0 +1,35 @@
+from operator import add
+
+with open('input.txt', 'r') as infile:
+    instructions = [line.strip() for line in infile]
+
+blackTiles = set()
+
+dirs = {
+    'e':    (1,-1,0),
+    'ne':   (1,0,-1),
+    'nw':   (0,1,-1),
+    'w':    (-1,1,0),
+    'sw':   (-1,0,1),
+    'se':   (0,-1,1)
+}
+
+for inst in instructions:
+    pos = [0,0,0]
+    while len(inst) > 0:
+        if inst[0] in ['e','w']:
+            direction = dirs[inst[0]]
+            inst = inst[1:]
+        else:
+            direction = dirs[inst[0:2]]
+            inst = inst[2:]
+        pos = list(map(add, pos, direction))
+
+    pos = tuple(pos)
+
+    if pos not in blackTiles: blackTiles.add(pos)
+    else: blackTiles.remove(pos)
+
+print(len(blackTiles))
+
+    
