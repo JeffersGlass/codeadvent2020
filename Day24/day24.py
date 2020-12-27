8794b87bf8c1928e55d6ac49b48af2fa31111b40
Jeff Glass <glass.jeffrey@gmail.com>
2020-12-27 09:35:08 -0600

    Clean up solution to day24 part 1

--- Day24/day24.py
+++ Day24/day24.py
@@ -1,35 +1,36 @@
 from operator import add
 
-with open('input.txt', 'r') as infile:
-    instructions = [line.strip() for line in infile]
+if __name__ == '__main__':
 
-blackTiles = set()
+    with open('input.txt', 'r') as infile:
+        instructions = [line.strip() for line in infile]
 
-dirs = {
-    'e':    (1,-1,0),
-    'ne':   (1,0,-1),
-    'nw':   (0,1,-1),
-    'w':    (-1,1,0),
-    'sw':   (-1,0,1),
-    'se':   (0,-1,1)
-}
+    blackTiles = set()
 
-for inst in instructions:
-    pos = [0,0,0]
-    while len(inst) > 0:
-        if inst[0] in ['e','w']:
-            direction = dirs[inst[0]]
-            inst = inst[1:]
-        else:
-            direction = dirs[inst[0:2]]
-            inst = inst[2:]
-        pos = list(map(add, pos, direction))
+    dirs = {
+        'e':    (1,-1,0),
+        'ne':   (1,0,-1),
+        'nw':   (0,1,-1),
+        'w':    (-1,1,0),
+        'sw':   (-1,0,1),
+        'se':   (0,-1,1)
+    }
 
-    pos = tuple(pos)
+    for inst in instructions:
+        pos = [0,0,0]
+        while len(inst) > 0:
+            if inst[0] in ['e','w']:
+                direction = dirs[inst[0]]
+                inst = inst[1:]
+            else:
+                direction = dirs[inst[0:2]]
+                inst = inst[2:]
+            pos = list(map(add, pos, direction))
 
-    if pos not in blackTiles: blackTiles.add(pos)
-    else: blackTiles.remove(pos)
+        pos = tuple(pos)
 
-print(len(blackTiles))
+        if pos not in blackTiles: blackTiles.add(pos)
+        else: blackTiles.remove(pos)
 
-    
+    print(f"Solution to part 1: {len(blackTiles)}")
+        
